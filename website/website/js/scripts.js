b3002b39fc6a875e9b2a12e3ba2af4f1a775768c
Burnysss <122602664+Burnysss@users.noreply.github.com>
2024-03-09 16:19:12 +0800

    Add files via upload

---
+++ website/website/js/scripts.js
@@ -0,0 +1,48 @@
+/*!
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            if (this.classList.contains('clicked')) {
+                this.blur(); // Remove focus when the same item is clicked again
+                this.classList.remove('clicked');
+            } else {
+                const prevClickedLink = document.querySelector('.nav-link.clicked');
+                if (prevClickedLink) {
+                    prevClickedLink.blur(); // Remove focus from the previously clicked item
+                    prevClickedLink.classList.remove('clicked');
+                }
+                this.classList.add('clicked');
+            }
+        });
+    });
+});
+
+
+
