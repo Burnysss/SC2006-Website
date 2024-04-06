80d67d1cd79318c341daddc7812d7fc92ae272fb
Burnysss <122602664+Burnysss@users.noreply.github.com>
2024-04-06 22:49:58 +0800

    Add files via upload

--- website/js/bookmark.js
+++ website/js/bookmark.js
@@ -1,25 +1,48 @@
 
-function showNotification() {
-    var notificationPopup = document.getElementById("notificationPopup");
-    notificationPopup.style.display = "block";
-    // Hide the notification after 3 seconds
-    setTimeout(function() {
-      hideNotification();
-    }, 1500); // 3000 milliseconds = 3 seconds
+// function showNotification() {
+//     var notificationPopup = document.getElementById("notificationPopup");
+//     notificationPopup.style.display = "block";
+//     // Hide the notification after 3 seconds
+//     setTimeout(function() {
+//       hideNotification();
+//     }, 1500); // 3000 milliseconds = 3 seconds
+//   }
+// function showNotification(event) {
+//   const clickedId = event.target.id;
+//   if (clickedId) {
+//       const index = clickedId.split("-")[1]; // Extract index from id
+//       const selectedItem = symbolData[index];
+//       const noti = document.getElementById('bookmarkstest');
+//       // Set the inner HTML of the addinfo paragraph tag with the desired information
+//       noti.innerHTML = `Symbol: ${selectedItem.symbol}, Type: ${selectedItem.type}, Data: ${selectedItem.data}` + "Bookmark Added";
+//       // Show the moreinfo div
+//       document.getElementById('notificationPopup').style.display = 'block';
+//           // Hide the notification after 3 seconds
+//       setTimeout(function() {
+//         hideNotification();
+//       }, 1500); // 3000 milliseconds = 3 seconds
+//   }
+// }
+function showNotification(event) {
+  const clickedId = event.target.id;
+  if (clickedId) {
+      const index = clickedId.split("-")[1]; // Extract index from id
+      const selectedItem = symbolData[index];
+      const noti = document.getElementById('bookmarkstest');
+      // Set the inner HTML of the addinfo paragraph tag with the desired information
+      noti.innerHTML = `Symbol: ${selectedItem.symbol}, Type: ${selectedItem.type}, Data: ${selectedItem.data}` + "Bookmark Added";
+      // Show the moreinfo div
+      document.getElementById('notificationPopup').style.display = 'block';
+          // Hide the notification after 3 seconds
+      setTimeout(function() {
+        hideNotification();
+      }, 1500); // 3000 milliseconds = 3 seconds
   }
-
+}
   function hideNotification() {
     var notificationPopup = document.getElementById("notificationPopup");
     notificationPopup.style.display = "none";
   }
 
-  function showMoreinfo() {
-    var popup = document.getElementById("moreinfo");
-    popup.style.display = "block";
-}
 
-function hideMoreinfo() {
-    var popup = document.getElementById("moreinfo");
-    popup.style.display = "none";
-}
 
--- website/js/search.js
+++ website/js/search.js
@@ -4,7 +4,6 @@
 
     // Array to store data associated with each symbol
     let symbolData = [];
-
     function getAlphaVantagedata() {
         const apiKey = "UABDDO1ICUMBFPZ2";
         const symbol = document.getElementById('searchtext2').value;
@@ -66,30 +65,50 @@
     }
 
     function updateUI(data) {
-    const divContents = document.getElementById('divContents');
-    divContents.innerHTML = '';
-    // Display data for each symbol
-    data.forEach(dataObj => {
-        const symbol = dataObj.symbol;
-        const type = dataObj.type;
-        divContents.innerHTML += `
-            <div class="filterDiv ${type} col-md-3 " id="itemlist">
-                <div class="center itemimgpad">
-                    <img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="W3Schools.com">
+        const divContents = document.getElementById('divContents');
+        divContents.innerHTML = '';
+        // Display data for each symbol
+        data.forEach((dataObj, index) => {
+            const symbol = dataObj.symbol;
+            const type = dataObj.type;
+    
+            divContents.innerHTML += `
+                <div class="filterDiv ${type} col-md-3 " id="itemlist">
+                    <div class="center itemimgpad">
+                        <img src="https://www.w3schools.com/images/w3schools_green.jpg" alt="W3Schools.com">
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h3 id="stockname">${symbol}</h3>
+                            <h3 id="stocktype">$:${dataObj.data}</h3>
+                        </div>
+                        <div class="col-md-4" >
+                        </div>
+                        <div class="col-md-4 center" >
+                            <button style="font-size:24px" class="hearts" id="index-${index}" onclick="showNotification(event)"><i class="fa fa-heart"></i></button>
+                        </div>
+                    </div>
+                    <a id="index-${index}" href="#" onclick="showMoreinfo(event)">View Details</a>
+
+
                 </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <h3 id="stockname">${symbol}</h3>
-                        <h3 id="stocktype">$:${dataObj.data}</h3>
-                    </div>
-                    <div class="col-md-4" >
-                    </div>
-                    <div class="col-md-4 center" >
-                        <button style="font-size:24px" id="hearts"><i class="fa fa-heart" onclick="showNotification()"></i></button>
-                    </div>
-                </div>
-                <a id="moreDetails" href="#" onclick="showMoreinfo()">View Details</a>
-            </div>
-            `;
-    });
-}
+                `;
+        });
+    }
+    function showMoreinfo(event) {
+        const clickedId = event.target.id;
+        if (clickedId) {
+            const index = clickedId.split("-")[1]; // Extract index from id
+            const selectedItem = symbolData[index];
+            const addinfo = document.getElementById('addinfo');
+            // Set the inner HTML of the addinfo paragraph tag with the desired information
+            addinfo.innerHTML = `Symbol: ${selectedItem.symbol}, Type: ${selectedItem.type}, Data: ${selectedItem.data}`;
+            // Show the moreinfo div
+            document.getElementById('moreinfo').style.display = 'block';
+        }
+    }
+    function hideMoreinfo() {
+        var popup = document.getElementById("moreinfo");
+        popup.style.display = "none";
+    }
+
